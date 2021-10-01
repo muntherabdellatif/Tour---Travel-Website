61d7d9af344b74acf0f491cb2e09252d8bae8992
monther saleh Abdellatif <monthersaleh366@gmail.com>
2021-10-01 11:43:45 +0300

    adding home , book ,package ,sevices and gallery sections

--- JS/index.js
+++ JS/index.js
@@ -1,12 +1,52 @@
 // start bar search 
 let searchBtn = document.querySelector("#search-btn");
 let searchBar = document.querySelector(".search-bar-container");
+let loginBtn = document.querySelector("#login-btn");
+let loginForm = document.querySelector(".login-form-container");
+let closeLoginForm =document.querySelector("#form-close");
+let menuBar =document.querySelector ("#menu-bar");
+let navBar = document.querySelector (".navbar");
+
 searchBtn.addEventListener ("click", ()=>{
+    menuBar.classList.remove("fa-times");
+    navBar.classList.remove("active");
+    loginForm.classList.remove("active");
     searchBtn.classList.toggle("fa-times");
     searchBar.classList.toggle("active");
 });
-window.onscroll= function () {
+menuBar.addEventListener ("click", ()=>{
     searchBtn.classList.remove("fa-times");
     searchBar.classList.remove("active");
+    loginForm.classList.remove("active");
+    menuBar.classList.toggle("fa-times");
+    navBar.classList.toggle("active");
+});
+loginBtn.addEventListener ("click", ()=>{
+    menuBar.classList.remove("fa-times");
+    navBar.classList.remove("active");
+    searchBtn.classList.remove("fa-times");
+    searchBar.classList.remove("active");
+    loginForm.classList.add("active");
+});
+closeLoginForm.addEventListener ("click", ()=>{
+    loginForm.classList.remove("active");
+});
+window.onscroll= function () {
+    menuBar.classList.remove("fa-times");
+    navBar.classList.remove("active");
+    searchBtn.classList.remove("fa-times");
+    searchBar.classList.remove("active");
+    loginForm.classList.remove("active");
 }
 // end bar search 
+// start home 
+let vidBtn =document.querySelectorAll(".vid-btn");
+vidBtn.forEach (btn =>{
+    btn.addEventListener("click",() => {
+        document.querySelector(".controls .active").classList.remove("active");
+        btn.classList.add("active");
+        let btnSrc =btn.getAttribute("data-btn");
+        document.querySelector("#video-slider").src=btnSrc;
+    });
+});
+// end home 
